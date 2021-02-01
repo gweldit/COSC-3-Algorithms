f66ecbb535c330548dcb35167a6e6e88e26a647a
unknown <gereweldit@gmail.com>
2021-02-01 20:18:50 +0400

    update insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,3 +1,4 @@
+
 class InsertionSort{
  constructor(arr){
      this.arr = arr;
@@ -5,27 +6,24 @@ class InsertionSort{
 
  sort(arr){
      this.arr = arr;
-     for (let j= arr.length-2; j>=0;j--){
+     for (let j = arr.length-2; j>=0;j--){
          let key = arr[j]
 
-         let i = j+1;
+         let i = j + 1;
          
-         while(i < arr.length +1 && arr[i]< key){
+         while(i < arr.length +1 && arr[i] < key){
              arr[i-1] = arr[i];
              i++
          }
          arr[i-1] = key;
 
-         
      }
-
  }
 
  print(){
      console.log(this.arr);
  }
 }
-
 
 function main(){
     let arr = [12,30,11,30,45,1,3,0,13,90];
@@ -34,7 +32,6 @@ function main(){
     inSort.sort(arr);
     inSort.print()
 }
-
 
 // start of running the code
 main();
